9c0f648a08ce91e33a06828f84a7460d59339de9
Herick Henrique <90839718+HerickHenriqueSS@users.noreply.github.com>
2022-03-10 22:33:39 -0300

    Add files via upload

--- EDVetor/EDVetor/src/Main.java
+++ EDVetor/EDVetor/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     
-    public static void main(String[] args) throws Exception {
-        Vetor vetor = new Vetor(5);
-        System.out.println(vetor);
+    public static void main(String[] args){
+    	
+    	try {
+	        Vetor vetor = new Vetor(5);
+	        vetor.adiciona("elemento 1");
+	        vetor.adiciona("elemento 2");
+	        System.out.println(vetor);
+	    }catch(Exception e) {
+	    	e.printStackTrace();
+	    }
     }
 }
--- EDVetor/EDVetor/src/Vetor.java
+++ EDVetor/EDVetor/src/Vetor.java
@@ -1,8 +1,53 @@
+import java.util.Arrays;
+
 public class Vetor {
-    private String[] elementos;
+    
+	private String[] elementos;
+	private int tamanho;
 
     public Vetor(int capaciodade){
         elementos = new String[capaciodade];
+        tamanho = 0;
 
     }
+   
+    
+   /* public void adiciona(String elemento) {
+    	for(int i=0; i < elementos.length; i++){
+    		if(elementos[i] == null) {
+    			elementos[i] = elemento;
+    		break;
+    		}
+    	}
+    }*/
+    
+    /*public void adiciona(String elemento) {
+    	if(tamanho < elementos.length) {
+    		elementos[tamanho] = elemento;
+    		tamanho ++;
+    	}else {
+    		throw new Exception("vetor esta cheio!!");
+    	}
+    }*/
+
+
+	@Override
+	public String toString() {
+		
+		StringBuffer s = new StringBuffer();
+		s.append("[");
+		for (int i = 0; i<tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(",");
+		}
+		
+		if(tamanho > 0) {
+			s.append(elementos[tamanho-1]);
+		}
+		
+		s.append("]");
+		return s.toString();
+		}
+    
 }
+
